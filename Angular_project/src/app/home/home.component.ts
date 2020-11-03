c7bf9472f7e6e3803846c23c0f20681841b0048d
nour <khaldinourelhouda2016@gmail.com>
2020-11-03 11:40:09 +0100

    mise à jour categorie

--- Angular_project/src/app/home/home.component.ts
+++ Angular_project/src/app/home/home.component.ts
@@ -9,7 +9,9 @@ import { Router } from '@angular/router';
 
 @Component({ 
     selector: 'app-home',
-    templateUrl: 'home.component.html' })
+    templateUrl: 'home.component.html' ,
+    styleUrls: ['./home.component.css']
+  })
 
 export class HomeComponent implements OnInit, OnDestroy {
     currentUser: boolean;
